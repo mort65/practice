6b2e76a141866d4b27fbb441810a88f1eb5b9ddb
Mort65 <Mort65@protonmail.com>
2018-08-28 01:48:56 +0430

    new file:   python/is_lab.py 	new file:   python/is_voodoo.py

---
+++ python/is_lab.py
@@ -0,0 +1,36 @@
+"""
+A lab number is a number such that the square of any of its prime divisors is still one of its divisors.
+For example:
+Input: 8Output: true (2 is a prime divisor of 8, and 2x2=4 is also a divisor of 8)
+Input: 50Output: true (5 is a prime divisor of 50, and 5x5=25 is also a divisor of 50)
+Write a program to check if the user input is a Lab number or not.
+Bonus: Print all the Lab numbers in a given range.
+"""
+
+def isprime(x):
+  for i in range(2,int(x/2)+1):
+    if x % i == 0:
+      return False
+  return True
+      
+def islab(x):
+  c = 0
+  for i in range(2,int(x / 2) + 1):
+    if (x % i) == 0:
+      if isprime(i):
+        if (x % (i ** 2)) != 0:
+          return False
+        else:
+          c += 1
+  if c > 0:
+    return True
+  else:
+    return False
+
+for i in range(2,101):
+  if islab(i):
+    print(str(i) + " : True")
+    
+input("\nPress any key to exit...")
+
+
---
+++ python/is_voodoo.py
@@ -0,0 +1,29 @@
+"""
+A Voodoo prime is a prime number whose reciprocal (in decimal) has the same number in its digits. For example, 7 is a voodoo prime because its reciprocal 1/7=0.14285714285 contains 7.
+Examples:Input: 3Output: true (1/3=0.33333333333 contains 3)
+Input: 11Output: false (1/11=0.0909090909 doesn't contain 11)
+Write a program to check if the user input is a Voodoo prime or not.
+Bonus: Print all the Voodoo primes in a given range.
+"""
+
+def is_prime(x):
+  if x > 1:
+    for i in range(2,int(x/2)+1):
+      if x % i == 0:
+        return False
+    return True
+  return False
+
+def is_voodoo(x):
+  if is_prime(x):
+    return (str(x) in str(1/x))
+  return False
+
+try:
+  for i in range(int(input())):
+    if is_voodoo(i):
+      print(str(i) + " : " + str(1/i))
+except Exception as e:
+  print(e)
+
+input("\nPress any key to exit...")
