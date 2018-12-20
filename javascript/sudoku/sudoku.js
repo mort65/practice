b0d7bec61151241b6ed0bd60185768bc97170289
Mort65 <Mort65@protonmail.com>
2018-12-21 01:38:01 +0330

    Update sudoku.js

--- javascript/sudoku/sudoku.js
+++ javascript/sudoku/sudoku.js
@@ -10,6 +10,64 @@ let redoes = []
 class Sudoku {
     constructor(values) {
         this.values = values;
+        this._values;
+        this.possible = [
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '', '']
+        ];
+        this.actual = [
+            [this.values[0], this.values[9], this.values[18],
+                this.values[27], this.values[36], this.values[45],
+                this.values[54], this.values[63], this.values[72]
+            ],
+
+            [this.values[1], this.values[10], this.values[19],
+                this.values[28], this.values[37], this.values[46],
+                this.values[55], this.values[64], this.values[73]
+            ],
+
+            [this.values[2], this.values[11], this.values[20],
+                this.values[29], this.values[38], this.values[47],
+                this.values[56], this.values[65], this.values[74]
+            ],
+
+            [this.values[3], this.values[12], this.values[21],
+                this.values[30], this.values[39], this.values[48],
+                this.values[57], this.values[66], this.values[75]
+            ],
+
+            [this.values[4], this.values[13], this.values[22],
+                this.values[31], this.values[40], this.values[49],
+                this.values[58], this.values[67], this.values[76]
+            ],
+
+            [this.values[5], this.values[14], this.values[23],
+                this.values[32], this.values[41], this.values[50],
+                this.values[59], this.values[68], this.values[77]
+            ],
+
+            [this.values[6], this.values[15], this.values[24],
+                this.values[33], this.values[42], this.values[51],
+                this.values[60], this.values[69], this.values[78]
+            ],
+
+            [this.values[7], this.values[16], this.values[25],
+                this.values[34], this.values[43], this.values[52],
+                this.values[61], this.values[70], this.values[79]
+            ],
+
+            [this.values[8], this.values[17], this.values[26],
+                this.values[35], this.values[44], this.values[53],
+                this.values[62], this.values[71], this.values[80]
+            ]
+        ];
         this.solve = () => {
             for (let i = 0; i < this.values.length; i++) {
                 for (let j = 0; j < this.values.length; j++) {
@@ -30,16 +88,77 @@ class Sudoku {
                     }
                 }
             }
-            if (this.values.filter(x => x === 0).length < 65) {
-                let result = this.solveSudoku(this.values);
-                if (result === undefined)
-                    return this.solveSudoku(this.values);
-                else {
-                    return result
+            let changes = false;
+            let exitLoop = false;
+            try {
+                do {
+                    do {
+                        do {
+                            do {
+                                changes = this.checkColumnAndRows();
+                                if (this.isPuzzleSolved()) {
+                                    exitLoop = true;
+                                    break;
+                                }
+                            } while (changes);
+
+                            if (exitLoop) break;
+
+                            changes = this.lookForLoneRangersinBlocks();
+                            if (this.isPuzzleSolved()) {
+                                exitLoop = true;
+                                break;
+                            }
+                        } while (changes);
+
+                        if (exitLoop) break;
+
+                        changes = this.lookForLoneRangersinRows();
+                        if (this.isPuzzleSolved()) {
+                            exitLoop = true;
+                            break;
+                        }
+                    } while (changes);
+
+                    if (exitLoop) break;
+
+                    changes = this.lookForLoneRangersinColumns();
+                    if (this.isPuzzleSolved()) {
+                        exitLoop = true;
+                        break;
+                    }
+                } while (changes);
+
+            } catch (ex) {
+                throw ex;
+            }
+            let tActual = transposeArray(this.actual, this.actual.length);
+            for (let i = 0; i < this.values.length; i++) {
+                this.values[i] = tActual[parseInt(i % 9)][parseInt(i / 9)];
+            }
+
+            if (this.isPuzzleSolved()) {
+                return true;
+            }
+
+            this._values = this.values;
+            if (this._values.filter(x => x === 0).length < 65) {
+                try {
+                    this.solveSudokuRecursivly(this._values);
+                } catch (err) {
+                    if (err === "Max Iterations") {
+                        return false;
+
+                    } else throw (err);
                 }
+                if (isPuzzleSolved(0)) {
+                    this.values = this._values;
+                    return true;
+                }
+                return false;
             } else {
                 return false
-            };
+            }
         };
     }
     sameRow(i, j) {
@@ -52,32 +171,227 @@ class Sudoku {
         return (parseInt(i / 27) === parseInt(j / 27) && parseInt(i % 9 / 3) === parseInt(j % 9 / 3));
     }
 
-    solveSudoku(v) {
-        let i = v.indexOf(0);
-        if (i === -1) {
-            let solved = true;
-            for (let j = 0; j < v.length; j++) {
-                if (!solved) {
-                    break;
+    calculatePossibleValues(col, row) {
+        let str;
+        if (this.possible[col][row] === '') {
+            str = '123456789';
+        } else str = this.possible[col][row];
+
+        //check by column
+        for (let r = 0; r < 9; r++) {
+            if (this.actual[col][r] !== 0) {
+                str = str.replace(this.actual[col][r].toString(), '');
+            }
+
+        }
+
+        //check by row
+        for (let c = 0; c < 9; c++) {
+            if (this.actual[c][row] !== 0) {
+                str = str.replace(this.actual[c][row].toString(), '');
+            }
+
+        }
+
+        //check by block
+        let startC = col - (col % 3);
+        let startR = row - (row % 3);
+        for (let rr = startR; rr <= startR + 2; rr++) {
+            for (let cc = startC; cc <= startC + 2; cc++) {
+                if (this.actual[cc][rr] !== 0) {
+                    str = str.replace(this.actual[cc][rr].toString(), '');
                 }
-                if (v[j]) {
-                    for (let k = 0; k < v.length; k++) {
-                        if (j !== k) {
-                            if (this.sameRow(j, k) || this.sameCol(j, k) || this.sameBlock(j, k)) {
-                                if (v[j] === v[k]) {
-                                    solved = false;
+            }
+        }
+
+        //if possible value is empty then throw invalid move
+        if (str === '') {
+            console.log(this.actual, col, row);
+            throw 'Invalid Move';
+        }
+        return str;
+    }
+
+    checkColumnAndRows() {
+        let changes = false;
+
+        //check all cells
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (this.actual[col][row] === 0) {
+                    try {
+                        this.possible[col][row] = this.calculatePossibleValues(col, row);
+                    } catch (ex) {
+                        console.log(ex);
+                        throw 'Invalid Move';
+                    }
+                    if (this.possible[col][row].length === 1) {
+                        //the number is confirmed
+                        //console.log(col, row, Number(possible[col][row]));
+                        this.actual[col][row] = parseInt(this.possible[col][row]);
+                        changes = true;
+                    }
+                }
+            }
+        }
+        return changes;
+    }
+
+    lookForLoneRangersinBlocks() {
+        let changes = false;
+        let nextBlock;
+        let occurrence;
+        let cPos, rPos;
+
+        //check for numbers 1 to 9
+        for (let n = 1; n < 10; n++) {
+
+            //check the 9 blocks
+            for (let r = 0; r < 9; r += 3) {
+                for (let c = 0; c < 9; c += 3) {
+                    nextBlock = false;
+
+                    //check within  the minigrid
+                    occurrence = 0;
+                    for (let rr = 0; rr < 3; rr++) {
+                        for (let cc = 0; cc < 3; cc++) {
+                            if (this.actual[c + cc][r + rr] === 0 && this.possible[c + cc][r + rr].length && this.possible[c + cc][r + rr].indexOf(n.toString()) > -1) {
+                                occurrence++;
+                                cPos = c + cc;
+                                rPos = r + rr;
+                                if (occurrence > 1) {
+                                    nextBlock = true;
                                     break;
                                 }
                             }
                         }
+                        if (nextBlock) break;
+                    }
+                    if (!nextBlock && occurrence === 1) {
+                        //the number confirmed
+                        //console.log(cPos, rPos, n);
+                        this.actual[cPos][rPos] = n;
+                        changes = true;
                     }
                 }
+
             }
 
-            if (solved) {
-                this.values = v;
+        }
+        return changes;
+    }
+
+    lookForLoneRangersinRows() {
+        let changes = false;
+        let occurrence;
+        let cPos, rPos;
+
+        //check by row
+        for (let r = 0; r < 9; r++) {
+            for (let n = 1; n < 10; n++) {
+                occurrence = 0;
+                for (let c = 0; c < 9; c++) {
+                    if (this.actual[c][r] === 0 && this.possible[c][r].length && this.possible[c][r].indexOf(n.toString()) > -1) {
+                        occurrence++;
+
+                        //if multiple occurrences, not a  lone ranger anymore
+                        if (occurrence > 1) break;
+                        cPos = c;
+                        rPos = r;
+                    }
+                }
+                if (occurrence === 1) {
+                    //number is confirmed
+                    //console.log(cPos, rPos, n);
+                    this.actual[cPos][rPos] = n;
+                    changes = true;
+                }
             }
-            return solved;
+        }
+        return changes;
+    }
+
+    lookForLoneRangersinColumns() {
+        let changes = false;
+        let occurrence;
+        let cPos, rPos;
+
+        //check by column
+        for (let c = 0; c < 9; c++) {
+            for (let n = 1; n < 10; n++) {
+                occurrence = 0;
+                for (let r = 0; r < 9; r++) {
+                    if (this.actual[c][r] === 0 && this.possible[c][r].length && this.possible[c][r].indexOf(n.toString()) > -1) {
+                        occurrence++;
+
+                        //if multiple occurrences, not a  lone ranger anymore
+                        if (occurrence > 1) break;
+                        cPos = c;
+                        rPos = r;
+                    }
+                }
+                if (occurrence === 1) {
+                    //number is confirmed
+                    //console.log(cPos, rPos, n);
+                    this.actual[cPos][rPos] = n;
+                    changes = true;
+                }
+            }
+        }
+        return changes;
+    }
+
+    isPuzzleSolved(m = 1) {
+        if (m) {
+            for (let c = 0; c < 9; c++) {
+                if (this.actual[c].indexOf(0) !== -1) return false;
+            }
+            return true;
+        } else {
+            let isSolved = true;
+            if (this._values.length === this.values.length) {
+                for (let j = 0; j < this._values.length; j++) {
+                    if (!isSolved) {
+                        break;
+                    }
+                    if (this._values[j]) {
+                        for (let k = 0; k < this._values.length; k++) {
+                            if (j !== k) {
+                                if (this.sameRow(j, k) || this.sameCol(j, k) || this.sameBlock(j, k)) {
+                                    if (this._values[j] === this._values[k]) {
+                                        isSolved = false;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        isSolved = false;
+                        break;
+                    }
+                }
+            } else {
+                isSolved = false;
+            }
+            return isSolved;
+        }
+    }
+
+    solveSudokuRecursivly(v) {
+        if (typeof this.solveSudokuRecursivly.iters == 'undefined')
+            this.solveSudokuRecursivly.iters = 1;
+        else {
+            if (this.solveSudokuRecursivly.iters > 99999) {
+                this.solveSudokuRecursivly.iters = undefined;
+                throw "Max Iterations";
+            }
+            this.solveSudokuRecursivly.iters++;
+        }
+        let i = v.indexOf(0);
+        if (i === -1) {
+            this._values = v;
+            this.solveSudokuRecursivly.iters = undefined;
+            return;
         }
         let excludedNumbers = [];
         for (let j = 0; j < this.values.length; j++) {
@@ -90,9 +404,33 @@ class Sudoku {
         for (let m of '123456789') {
             if (excludedNumbers.indexOf(parseInt(m)) === -1) {
                 v = v.slice(0, i).concat(parseInt(m), v.slice(i + 1, v.length));
-                this.solveSudoku(v)
+                this.solveSudokuRecursivly(v);
             }
         }
+    }
+}
+
+solve = () => {
+    let start = performance.now();
+    let solved = false
+    solved = sudoku.solve();
+    if (!solved && defaultValues.length) {
+        let s = new Sudoku(defaultValues);
+        solved = s.solve();
+        if (solved) {
+            sudoku.values = s.values;
+        }
+    }
+    let end = performance.now();
+    if (solved) {
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].innerText = sudoku.values[i];
+            cells[i].style.backgroundColor = "white";
+        }
+        undoes = [];
+        redoes = [];
+        selectedIndex = null;
+        caption.innerText = `Solved in ${end - start} ms.`;
     }
 }
 
@@ -125,6 +463,21 @@ select = index => {
         cells[selectedIndex].style.backgroundColor = "white";
     }
     selectedIndex = index;
+}
+
+function transposeArray(array, arrayLength) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        newArray.push([]);
+    };
+
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < arrayLength; j++) {
+            newArray[j].push(array[i][j]);
+        };
+    };
+
+    return newArray;
 }
 
 normalize = n => {
@@ -338,30 +691,6 @@ reset = () => {
     redoes = [];
 }
 
-solve = () => {
-    let start = performance.now();
-    let solved = false
-    solved = sudoku.solve();
-    if (!solved && defaultValues.length) {
-        let s = new Sudoku(defaultValues);
-        solved = s.solve();
-        if (solved) {
-            sudoku.values = s.values;
-        }
-    }
-    let end = performance.now();
-    if (solved) {
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].innerText = sudoku.values[i];
-            cells[i].style.backgroundColor = "white";
-        }
-        undoes = [];
-        redoes = [];
-        selectedIndex = null;
-        caption.innerText = `Solved in ${end - start} ms.`;
-    }
-}
-
 readFile = e => {
     let file = e.files[0];
     if (!file) {
@@ -373,9 +702,10 @@ readFile = e => {
         let values = [];
         let isBold = true;
         for (let i = 0; i < content.length; i++) {
-            if ('1234567890*'.indexOf(content[i] > -1)) {
+            if ('1234567890.*'.indexOf(content[i] > -1)) {
                 switch (content[i]) {
                     case '0':
+                    case '.':
                         values.push(0);
                         isBold = true;
                         break;
